bbb177d41d1c173d94579d7e80ae030b86ce46be
luiscl1495 <luis141495@gmail.com>
2026-09-19 02:20:54 -0500

    Funcionalidad-Movimientos

--- Angular/src/app/nuevo-movimiento/nuevo-movimiento.component.ts
+++ Angular/src/app/nuevo-movimiento/nuevo-movimiento.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Movimiento } from '../models/movimiento.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   selector: 'app-nuevo-movimiento',
   styleUrl: './nuevo-movimiento.component.css',
   templateUrl: './nuevo-movimiento.component.html',
 })
-export class NuevoMovimientoComponent {}
+
+export class NuevoMovimientoComponent {
+
+  saldo = input.required<number>();
+  nuevoMovimiento = output<Movimiento>();
+  tipo: string = '';
+  monto: number = 0;
+
+  esInvalido(): boolean {
+  if (!this.tipo || this.monto <= 0) return true;
+  if (this.tipo === 'Gasto' && this.monto > this.saldo()) return true;
+  return false;
+}
+
+  registrar() {
+  if (!this.esInvalido()) {
+    const hoy = new Date();
+    const fechaStr = hoy.toLocaleDateString('es-PE', { day: '2-digit', month: '2-digit', year: 'numeric' });
+    this.nuevoMovimiento.emit({
+      fecha: fechaStr,
+      tipo: this.tipo as 'Recarga' | 'Gasto',
+      monto: Number(this.monto)
+    });
+    this.monto = 0;
+    this.tipo = '';
+  }
+  }
+}
+
+
+
