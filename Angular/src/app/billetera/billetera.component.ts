f348876af10de80edadc36865eb824777a037b00
luiscl1495 <luis141495@gmail.com>
2026-09-19 02:19:46 -0500

    Funcionalidad-Movimientos

--- Angular/src/app/billetera/billetera.component.ts
+++ Angular/src/app/billetera/billetera.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NuevoMovimientoComponent } from '../nuevo-movimiento/nuevo-movimiento.component';
+import { Movimiento } from '../models/movimiento.model';
 
 @Component({
-  imports: [NuevoMovimientoComponent],
+  imports: [NuevoMovimientoComponent, CommonModule],
   selector: 'app-billetera',
   styleUrl: './billetera.component.css',
   templateUrl: './billetera.component.html',
 })
-export class BilleteraComponent {}
+export class BilleteraComponent {
+  
+  saldo: number = 250.00;
+  movimientos: Movimiento[] = [
+    { fecha: '18/09/2026', tipo: 'Recarga', monto: 100.00 },
+    { fecha: '17/09/2026', tipo: 'Gasto', monto: 24.51 },
+    { fecha: '15/09/2026', tipo: 'Gasto', monto: 75.49 },
+    { fecha: '12/09/2026', tipo: 'Recarga', monto: 250.00 }
+  ];
+
+  procesarMovimiento(mov: Movimiento) {
+    this.movimientos.unshift(mov);
+    if (mov.tipo === 'Recarga') {
+      this.saldo += mov.monto;
+    } else if (mov.tipo === 'Gasto') {
+      this.saldo -= mov.monto;
+    }
+
+  }
+
+  
+}
